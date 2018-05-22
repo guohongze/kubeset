655383337ca51d1c770f4a5be09a26440e98a39c
guohongze <guohongze@126.com>
2018-05-22 10:31:32 +0800

    structure update

--- borg.py
+++ borg.py
@@ -7,7 +7,7 @@ import click
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 from lib.deployment import Deployment
-from pprint import pprint
+
 
 def k8s_init():
     config.load_kube_config()
@@ -51,38 +51,6 @@ def deployment(ns):
 
 
 @click.command()
-@click.option('--name', help='service name')
-@click.option('--ns', help='namespace name')
-def create_svc(name, ns="default"):
-    api_instance = client.CoreV1Api()
-    svc = client.V1Service()
-    svc.api_version = "v1"
-    svc.kind = "Service"
-    svc.metadata = client.V1ObjectMeta(name=name)
-    spec = client.V1ServiceSpec()
-    spec.selector = {"app": "nginx-dep"}
-    spec.type = "NodePort"
-    spec.ports = [client.V1ServicePort(protocol="TCP", port=80, target_port=80)]
-    svc.spec = spec
-    api_response = api_instance.create_namespaced_service(namespace=ns, body=svc)
-    print("Deployment updated. status='%s'" % str(api_response.status))
-
-
-@click.command()
-@click.option('--name', help='deployment name')
-@click.option('--img', help='docker images address')
-@click.option('--replicas', help='pod replica number')
-@click.option('--ns', default="default", help='namespace name')
-def update(name, replicas, img, ns):
-    api_instance = Deployment(name, ns, replicas, img)
-    if img:
-        update_response = api_instance.update()
-    if replicas:
-        update_response = api_instance.update()
-    return update_response
-
-
-@click.command()
 @click.option('--name', help='deployment name')
 @click.option('--img', default="nginx:1.11", help='docker images address')
 @click.option('--replicas', default=1, help='pod replica number')
@@ -99,7 +67,23 @@ def create_dep(name, replicas, img, ns):
 def delete_dep(name, ns):
     api_instance = Deployment(name, ns)
     delete_response = api_instance.delete()
+    if delete_response:
+        print(delete_response)
     return delete_response
+
+
+@click.command()
+@click.option('--name', help='deployment name')
+@click.option('--img', help='docker images address')
+@click.option('--replicas', help='pod replica number')
+@click.option('--ns', default="default", help='namespace name')
+def update(name, replicas, img, ns):
+    api_instance = Deployment(name, ns, replicas, img)
+    if img:
+        update_response = api_instance.update()
+    if replicas:
+        update_response = api_instance.update()
+    return update_response
 
 
 @click.command()
@@ -137,6 +121,23 @@ def view(name, ns):
     except ApiException as e:
         print("Exception when calling CoreV1Api->read_namespaced_service:% s\n" % e)
 
+
+@click.command()
+@click.option('--name', help='service name')
+@click.option('--ns', help='namespace name')
+def create_svc(name, ns="default"):
+    api_instance = client.CoreV1Api()
+    svc = client.V1Service()
+    svc.api_version = "v1"
+    svc.kind = "Service"
+    svc.metadata = client.V1ObjectMeta(name=name)
+    spec = client.V1ServiceSpec()
+    spec.selector = {"app": "nginx-dep"}
+    spec.type = "NodePort"
+    spec.ports = [client.V1ServicePort(protocol="TCP", port=80, target_port=80)]
+    svc.spec = spec
+    api_response = api_instance.create_namespaced_service(namespace=ns, body=svc)
+    print("Deployment updated. status='%s'" % str(api_response.status))
 
 if __name__ == '__main__':
     cli.add_command(pods)
